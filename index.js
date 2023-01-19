8043c887eaeab13334842aede7064ee61feddb1a
dikawibisono <dikawibisono98@gmail.com>
2023-01-19 23:42:34 +0700

    modify index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log("test 1")
 console.log("test 2")
 console.log("test 3")
+
+
+console.log("test 4")
+console.log("test 5")
+console.log("test 6")
+console.log("test 7")
+console.log("test 8")
