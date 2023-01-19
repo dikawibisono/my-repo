231480aec4d3c799d2fe5c35f40c7309934affeb
dikawibisono <dikawibisono98@gmail.com>
2023-01-19 23:53:54 +0700

    change2

--- call.js
+++ call.js
@@ -1,2 +1,8 @@
 console.log("this is feature call")
 console.log("you can call me if you need something")
+
+console.log("this is feature call")
+console.log("you can call me if you need something")
+
+console.log("this is feature call")
+console.log("you can call me if you need something")
