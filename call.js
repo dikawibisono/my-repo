8f9409722e3c284d07b8eb3969658016a6de47e7
dikawibisono <97390732+dikawibisono@users.noreply.github.com>
2023-01-19 23:56:19 +0700

    feature: call (#1)
    * change1
    
    * change2
    
    * change3

---
+++ call.js
@@ -0,0 +1,27 @@
+console.log("this is feature call")
+
+console.log("you can call me if you need something")
+
+console.log("this is feature call")
+
+console.log("you can call me if you need something")
+
+console.log("this is feature call")
+
+console.log("you can call me if you need something")
+
+console.log("this is feature call")
+
+console.log("you can call me if you need something")
+
+console.log("this is feature call")
+
+console.log("you can call me if you need something")
+
+console.log("this is feature call")
+
+console.log("you can call me if you need something")
+
+console.log("this is feature call")
+
+console.log("you can call me if you need something")
